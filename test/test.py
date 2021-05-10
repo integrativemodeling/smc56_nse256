299a0c3b42222f872b7f501f8e5771e3011226e2
Ben Webb <ben@salilab.org>
2021-05-10 16:01:39 -0700

    Test generation of mmCIF file

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+import ihm.reader
+import sys
 import os
 import glob
 import shutil
@@ -5,13 +7,17 @@ import subprocess
 import unittest
 import RMF
 
-MODELLER_DIR = os.path.abspath("../non_cc_comparative_modeling/smc56_head_MODELLER")
+TOPDIR = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+
+MODELLER_DIR = os.path.join(TOPDIR, "non_cc_comparative_modeling",
+                            "smc56_head_MODELLER")
 N_COMPARATIVE_MODELS = 20
 
-INTEGRATIVE_MODELING_DIR = os.path.abspath("../integrative_modeling")
-DATADIR = os.path.abspath("../data")
+INTEGRATIVE_MODELING_DIR = os.path.join(TOPDIR, "ntegrative_modeling")
+DATADIR = os.path.join(TOPDIR, "data")
 N_WARMUP_MODELS = 20
 N_PRODUCTION_MODELS = 20
+
 
 class Tests(unittest.TestCase):
     def test_smc56_head_model(self):
@@ -35,8 +41,47 @@ class Tests(unittest.TestCase):
         
         # switch to current dir
         os.chdir(currdir)
-    
-    
+
+    def test_mmcif(self):
+        """Test generation of mmCIF output"""
+        mmcif = 'smc56_nse256.cif'
+        os.chdir(os.path.join(TOPDIR, 'archiving'))
+        if os.path.exists(mmcif):
+            os.unlink(mmcif)
+        # Potentially override methods that need network access
+        env = os.environ.copy()
+        env['PYTHONPATH'] = \
+            os.path.join(TOPDIR, 'test', 'mock') + ':' \
+            + env.get('PYTHONPATH', '')
+        subprocess.check_call(["python", 'archiving.py'], env=env)
+        self._check_mmcif(mmcif)
+
+    def _check_mmcif(self, fname):
+        with open(fname) as fh:
+            s, = ihm.reader.read(fh)
+        self.assertEqual(len(s.citations), 0)
+        self.assertEqual(len(s.software), 4)
+        self.assertEqual(len(s.orphan_starting_models), 27)
+        # Should be 1 state
+        self.assertEqual(len(s.state_groups), 1)
+        state1, = s.state_groups[0]
+        # Should be 1 model
+        self.assertEqual(sum(len(x) for x in state1), 1)
+        # Check # of spheres and atoms in one model
+        m = state1[0][0]
+        self.assertEqual(len(m._spheres), 2805)
+        self.assertEqual(len(m._atoms), 0)
+        # Should be 1 ensemble
+        self.assertEqual([e.num_models for e in s.ensembles], [29975])
+        # Three restraints - crosslinks
+        xl1, xl2, xl3 = s.restraints
+        self.assertEqual(len(xl1.experimental_cross_links), 159)
+        self.assertEqual(len(xl1.cross_links), 159)
+        self.assertEqual(len(xl2.experimental_cross_links), 109)
+        self.assertEqual(len(xl2.cross_links), 109)
+        self.assertEqual(len(xl3.experimental_cross_links), 67)
+        self.assertEqual(len(xl3.cross_links), 67)
+
     def test_integrative_model(self):
         """
         Test if IMP structural sampling on the Smc5/6 pentamer is successful
@@ -71,4 +116,3 @@ class Tests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    
